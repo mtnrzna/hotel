2321a4602fcc29cff88c854a7e26ca70e80d00fa
matinrezania <matin.rezania@gmail.com>
2023-01-07 23:12:18 -0800

    finish gallery and about-us page

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../responsive";
 
 const ContainerDiv = styled.div`
     display: flex;
@@ -7,6 +8,11 @@ const ContainerDiv = styled.div`
     justify-content: center;
     background-color: #f6f6f6;
     overflow: hidden;
+    ${mobile({
+        width: "100%",
+        padding: "60px 0",
+        backgroundColor: "white",
+    })}
 `;
 
 const Container = ({ children, style }) => {
--- src/components/GalleryPage/GalleryGrid.jsx
+++ src/components/GalleryPage/GalleryGrid.jsx
@@ -1,22 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 import Container from "../Container";
 import Wrapper from "../Wrapper";
-import styled from "styled-components";
+import { mobile } from "../../responsive";
 
 const Top = styled.h3`
     margin-bottom: 20px;
+    ${mobile({
+        display: "none",
+    })}
 `;
 
 const Bottom = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    ${mobile({
+        width: "100%",
+    })}
 `;
 const Grid = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    ${mobile({
+        width: "100%",
+    })}
 `;
 
 const Column = styled.div`
@@ -24,18 +34,33 @@ const Column = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    ${mobile({
+        height: "100px",
+        width: "100%",
+        display: "block",
+    })}
 `;
 
 const ImageContainer = styled.div`
     max-height: 350px;
     margin: 10px;
     overflow: hidden;
+    ${mobile({
+        height: "200px",
+        width: "100%",
+        margin: "10px 0",
+        ObjectFit: "fill",
+    })}
 `;
 
 const Image = styled.img`
     max-height: 100%;
     max-width: 100%;
     object-fit: cover;
+    ${mobile({
+        height: "100%",
+        width: "100%",
+    })}
 `;
 
 const GalleryGrid = () => {
